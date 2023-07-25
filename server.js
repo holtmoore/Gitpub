9f06ecd100459d7fc6d4b3e02bb477b1d2853037
Holt Moore <holt.w.moore@gmail.com>
2023-07-25 14:28:09 -0500

    final commit

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Require our mock database
+
 const drinks = require('./models/drinks.js');
 const food = require("./models/food.js");
 
 
 
 
-// Set EJS as the view engine
+
 app.set('view engine', 'ejs');
 
-// Define routes
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the Gitpub App!');
-// });
 
 app.get('/drinks', (req, res) => {
   res.render('drinks_index', { drinks: drinks });
